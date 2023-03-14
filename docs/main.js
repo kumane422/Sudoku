412001295dc891f142540ace0c597ef588b06ce8
kumane422 <127856842+kumane422@users.noreply.github.com>
2023-03-15 02:56:38 +0900

    画像認識

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,12 @@
-alert("hello")
+const main = async () => {
+  const buf = document.querySelector("#img")
+  const { data: { text } } = await Tesseract.recognize(buf, "eng", {
+    logger: function(m) {
+      console.log(m.status)
+    }
+  })
+
+  document.querySelector("#result").textContent = text
+}
+
+window.onload = main()
