55e06a519919809178e841f6c9d39cd7b2780630
kumane422 <127856842+kumane422@users.noreply.github.com>
2023-03-17 19:33:10 +0900

    画像をアップロードして文字認識

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,20 @@
-const main = async () => {
+const main = () => {
+  console.log("Start")
+}
+
+const loadImage = (obj) => {
+  for(const file of obj.files) {
+    const fileReader = new FileReader()
+    fileReader.onload = (e => {
+      const img = document.getElementById("img")
+      img.src = e.target.result
+    })
+    fileReader.readAsDataURL(file)
+  }
+  kaiseki()
+}
+
+const kaiseki = async () => {
   const buf = document.querySelector("#img")
   const { data: { text } } = await Tesseract.recognize(buf, "eng", {
     logger: function(m) {
