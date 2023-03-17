a47d0d7fd02c53689a700801ced57022fecebe4d
kumane422 <127856842+kumane422@users.noreply.github.com>
2023-03-17 21:54:24 +0900

    数独画像を読み取って問題の配列を用意

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,17 @@
 const previewCanvas = document.getElementById("preview")
 const recognizeCanvas = document.getElementById("recognize")
+const array = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
 const main = () => {
   const upload = document.getElementById("upload")
   upload.addEventListener("change", e => {
@@ -10,6 +22,12 @@ const main = () => {
 
 const loaded = async file => {
   const prev = await previewImage(file)
+  const img = await loadImage(prev)
+  await imageToCanvas(img)
+
+  const text = await recognize()
+  setQuizArray(text)
+  console.log(array)
 }
 
 // アップロードされた画像をプレビュー表示
@@ -22,8 +40,65 @@ const previewImage = async file => {
     }
     fileReader.readAsDataURL(file)
   })
-  }
 }
+
+// ファイルロード
+const loadImage = async src => {
+  return new Promise(resolve => {
+    const img = new Image()
+    img.onload = () => resolve(img)
+    img.src = src
+  })
+}
+
+// 数字か空欄か
+const isNum = (sx, sy, sw, sh) => {
+  const ctx = recognizeCanvas.getContext("2d")
+  let ave = 0
+  for (let x = sx; x < sx + sw; x++) {
+    for (let y = sy; y < sy + sh; y++) {
+      const { data: [r, g, b] } = ctx.getImageData(x, y, 1, 1)
+      ave += (255 - r + 255 - g + 255 - b)
+    }
+  }
+  ave /= sw * sh
+  return ave > 30 // 30は適当?
+}
+
+// アップロードされた画像から解析用に調整してCanvas表示
+const imageToCanvas = async image => {
+  return new Promise(resolve => {
+    recognizeCanvas.getContext("2d").drawImage(image, 0, 0, image.width, image.height, 0, 0, recognizeCanvas.width, recognizeCanvas.height)
+    const iw = image.width / 9
+    const ih = image.height / 9
+    const cw = recognizeCanvas.width / 9
+    const ch = recognizeCanvas.height / 9
+    const d = recognizeCanvas.width / image.width - 0.11
+    const ctx = recognizeCanvas.getContext("2d")
+    ctx.fillStyle = "blue"
+    ctx.font = `${Math.floor(cw * 1)}px sans-serif`
+    ctx.textAlign = "center"
+    ctx.textBaseline = "middle"
+    for(let i = 0; i < 9; i++) {
+      for(let j = 0; j < 9; j++) {
+        const n = 
+        ctx.drawImage(
+          image,
+          iw * (j + (1 - d) / 2), ih * (i + (1 - d) / 2),
+          iw * d,
+          ih * d,
+          cw * j,
+          ch * i,
+          cw,
+          ch
+        )
+        if (!isNum(cw * j, ch * i, cw, ch)) {
+          ctx.fillText("0", cw * (j + 0.5), ch * (i + 0.5))
+        }
+      }
+    }
+    resolve()
+  })
 }
 
 // クイズ画像に対する文字認識
@@ -33,8 +108,20 @@ const recognize = async () => {
       console.log(m.status)
     }
   })
+  return text
+}
 
-  document.querySelector("#result").textContent = text
+// 解析した文字列を問題の初期状態の形にする
+const setQuizArray = (text = "") => {
+  text = text.replace(" ", "").replace("　", "")
+  let t = 0
+  for(let i = 0; i < 9; i++) {
+    for(let j = 0; j < 9; j++) {
+      array[i][j] = text[t] == 0 ? "0" : text[t]
+      t++
+    }
+    t++
+  }
 }
 
 window.onload = main()
